89fc5984b9c9d65056fb2d232835ebeceb7018a2
Jan Kuta <jan.kuta@email.cz>
2023-09-10 00:35:10 +0200

    ssl require

--- lib/db.ts
+++ lib/db.ts
@@ -1,5 +1,5 @@
 import postgres from 'postgres'
 
-const sql = postgres(process.env.POSTGRES_URL as string)
+const sql = postgres(process.env.POSTGRES_URL as string + '?sslmode=require')
 
 export default sql
