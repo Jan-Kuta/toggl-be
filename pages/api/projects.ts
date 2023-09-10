0ca139fd65e16c8f1839ed7e1753eab5cff2e92b
Jan Kuta <jan.kuta@email.cz>
2023-09-10 16:13:19 +0200

    remove cookies

--- pages/api/projects.ts
+++ pages/api/projects.ts
@@ -6,7 +6,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Project[] | string>
 ) {
-  const userName = req.cookies.userName
+  const userName = req.query.userName
   if (!userName) {
     return res.status(401).send('Unauthorized')
   }
