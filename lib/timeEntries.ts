1c158e8693ee9db80a8f94e10698f6be09cf0890
Jan Kuta <jan.kuta@email.cz>
2023-09-11 09:25:12 +0200

    TE - fix

--- lib/timeEntries.ts
+++ lib/timeEntries.ts
@@ -12,42 +12,42 @@ export interface TimeEntry{
 
 export async function listByProject(userName: string, projectId: number) {
   return await sql<TimeEntry[]>`
-    SELECT t.id, t.task, t.start, t.end, t.project_id, p.project_name FROM time_entries t INNER JOIN projects p on time_entries.project_id = projects.id WHERE user_name=${userName} AND project_id=${projectId}
+    SELECT t.id, t.task, t.start, "end", t.project_id, p.project_name FROM time_entries t INNER JOIN projects p on time_entries.project_id = projects.id WHERE user_name=${userName} AND project_id=${projectId}
     ORDER BY start DESC
   `
 }
 
 export async function listStartsInInterval(userName: string, start: Date, end: Date) {
   return await sql<TimeEntry[]>`
-    SELECT t.id, t.task, t.start, t.end, t.project_id, p.project_name FROM time_entries t INNER JOIN projects p on time_entries.project_id = projects.id WHERE user_name=${userName} AND start >= ${start} AND start <= ${end}
+    SELECT t.id, t.task, t.start, "end", t.project_id, p.project_name FROM time_entries t INNER JOIN projects p on time_entries.project_id = projects.id WHERE user_name=${userName} AND start >= ${start} AND start <= ${end}
     ORDER BY start DESC
   `
 }
 
 export async function list(userName: string) {
   return await sql<TimeEntry[]>`
-    SELECT t.id, t.task, t.start, t.end, t.project_id, p.project_name FROM time_entries t INNER JOIN projects p on time_entries.project_id = projects.id WHERE user_name=${userName}
+    SELECT t.id, t.task, t.start, "end", t.project_id, p.project_name FROM time_entries t INNER JOIN projects p on time_entries.project_id = projects.id WHERE user_name=${userName}
     ORDER BY start DESC
   `
 }
 
 export async function create(te: TimeEntry) {
   return await sql<TimeEntry[]>`
-    INSERT INTO projects (task, start, end, user_name, project_id) VALUES (${te.task}, ${te.start}, ${te.end || null}, ${te.user_name})
-    RETURNING id, task, start, end, project_id
+    INSERT INTO projects (task, start, "end", user_name, project_id) VALUES (${te.task}, ${te.start}, ${te.end || null}, ${te.user_name})
+    RETURNING id, task, start, "end", project_id
   `
 }
 
 export async function update(te: TimeEntry) {
   return await sql<TimeEntry[]>`
-    UPDATE time_entry SET task=${te.task}, start={te.start}, end={te.end}, user_name=${te.user_name}, project_id=${te.project_id} WHERE id=${te.id}
-    RETURNING id, task, start, end, project_id
+    UPDATE time_entry SET task=${te.task}, start={te.start}, "end"={te.end}, user_name=${te.user_name}, project_id=${te.project_id} WHERE id=${te.id}
+    RETURNING id, task, start, "end", project_id
   `
 }
 
 export async function remove(te: TimeEntry) {
   return await sql<TimeEntry[]>`
     DELETE FROM time_entries WHERE id=${te.id}
-    RETURNING id, task, start, end, project_id
+    RETURNING id, task, start, "end", project_id
   `
 }
